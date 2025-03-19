ad08918682efea2a0b0ebe2a4c23e46adf152a88
Noé <75133369+noebsc@users.noreply.github.com>
2025-03-19 22:20:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+import { getDatabase, ref, set, push, get } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-storage.js";
 
 // 📌 Remplace par tes propres infos Firebase
